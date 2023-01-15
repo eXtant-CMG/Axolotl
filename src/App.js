0115f8ebca49f575c576b6ffdb8f15cdfe557afe
NoonShin <nooshin.shahidzadeh@gmail.com>
2023-01-15 17:42:25 +0100

    Views updated.

--- src/App.js
+++ src/App.js
@@ -15,23 +15,7 @@ import CodeMirrorCollab from "./CodeMirrorCollab";
 
 function App() {
     return (
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Edit <code>src/App.js</code> and save to reload.
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
         <XMLViewer />
-        // </div>
     );
 }
 
@@ -47,7 +31,7 @@ function XMLViewer() {
         <Container className="mt-5 vh-100">
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">XML Viewer</Navbar.Brand>
+                    <Navbar.Brand href="#home">Axolotl XML</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
@@ -70,18 +54,17 @@ function XMLViewer() {
             </Navbar>
             <Row className="h-50 g-5 pt-3">
                 <Col>
-                    <div className="border bg-light p-3">This is where the text goes
-                        <h1>{message}</h1>
+                    <div className="border bg-light p-3">
+                        {/*<h1>{message}</h1>*/}
                         <CodeMirrorCollab/>
                         {/*<Editor/>*/}
-                        {/*<Image src={pic} className="img-fluid p-3"></Image>*/}
                     </div>
                 </Col>
-                <Col>
-                    <div className="border bg-light h-100 p-3">This is where the image goes
-                        <AnnotationContainer  />
-                    </div>
-                </Col>
+                {/*<Col>*/}
+                {/*    <div className="border bg-light h-100 p-3">*/}
+                {/*        <AnnotationContainer  />*/}
+                {/*    </div>*/}
+                {/*</Col>*/}
             </Row>
         </Container>
     )
--- src/CodeMirrorCollab.js
+++ src/CodeMirrorCollab.js
@@ -2,7 +2,7 @@ import React, {useEffect, useRef, useState} from "react";
 import {basicSetup, EditorView} from "codemirror"
 import {ViewPlugin} from "@codemirror/view"
 import {xml} from "@codemirror/lang-xml"
-import {ChangeSet, EditorState} from "@codemirror/state"
+import {ChangeSet, EditorState, StateField} from "@codemirror/state"
 import {receiveUpdates, sendableUpdates, collab, getSyncedVersion} from "@codemirror/collab"
 import {io} from 'socket.io-client'
 import schema from './util/jsonSchema.json'
@@ -10,9 +10,23 @@ import {XMLValidator} from "fast-xml-parser";
 import {Decoration} from "@codemirror/view"
 
 const baseTheme = EditorView.baseTheme({
-    ".error-line": {backgroundColor: "coral"}
+    ".error-line": {backgroundColor: "#e2582266"},
+    ".changed-code": {backgroundColor: "#ac97e7cc"}
 })
 const errorLineDeco = Decoration.line({ class: "error-line" });
+
+const changedCodeMark = Decoration.mark({class: "changed-code"})
+
+const changedCodeField = StateField.define({
+        create() {
+            return Decoration.none;
+        },
+        update(section) {
+            return section
+        },
+        provide: field => EditorView.decorations.from(field)
+    }
+)
 
 const socket = io("http://127.0.0.1:5000");
 // socket.on("connect", () => {
@@ -48,6 +62,7 @@ function errorCheckAndValidationExtension(setValidation) {
             }
         }
 
+        // Add highlight to an error line
         getDeco(err) {
             let deco = []
             if (err.err.line) {
@@ -63,6 +78,8 @@ function errorCheckAndValidationExtension(setValidation) {
     return [plugin]
 }
 
+// Extension for updating the editor using built-in operational transformation
+// functions in CodeMirror and syncing everything with the server
 function updateExtension(startVersion, sock) {
     let pluginVersion = startVersion;
     let plugin = ViewPlugin.fromClass(class {
@@ -72,7 +89,9 @@ function updateExtension(startVersion, sock) {
                 // console.log("These are the updates I just received! " + sock.id)
                 // console.log(updates)
                 // console.log("This is my current version: " + getSyncedVersion(this.view.state))
-                this.view.dispatch(receiveUpdates(this.view.state, this.makeUpdates(updates)))
+                let changes = receiveUpdates(this.view.state, this.makeUpdates(updates))
+                this.view.dispatch(changes)
+                this.changeHighlight(changes.changes.desc.sections)
                 // console.log("This is my next version: " + getSyncedVersion(this.view.state))
                 // console.log(receiveUpdates(this.view.state, this.makeUpdates(updates)))
             });
@@ -96,6 +115,32 @@ function updateExtension(startVersion, sock) {
                 changes: ChangeSet.fromJSON(u.changes),
                 clientID: u.clientID
             }))
+        }
+
+        changeHighlight(sections) {
+            console.log("adding highlight")
+            let index = 0;
+            for (let i = 0; i < sections.length; i += 2) {
+                console.log(sections[i], sections[i+1]);
+                // This section has not changed
+                if (sections[i+1] === -1) {
+                    index = sections[i];
+                }
+                // This section has been deleted
+                else if (sections[i+1] === 0) {
+                    console.log("I will mark " + index + " as deleted")
+                }
+                // This section has had an insertion or a replacement
+                else {
+                    console.log("I will mark " + index + " as changed")
+                    let effect = changedCodeMark.range(index, index+sections[i+1])
+                    console.log(effect)
+                    this.view.dispatch({effect})
+                }
+            }
+            // sections.forEach(function (section) {
+            //     console.log(section);
+            // });
         }
 
     })
@@ -130,8 +175,8 @@ export default function CodeMirrorCollab() {
 
     return (
         <div>
-            <div id="validation-message">{validation.err.msg}</div>
             <div ref={editor}></div>
+            <div id="validation-message">{validation.err.msg}</div>
         </div>
     );
 }
